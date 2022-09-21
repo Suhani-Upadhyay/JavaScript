73e9a68c169b8ccc75fb259888535c3975caea72
Suhani Upadhyay <113186758+Suhani-Upadhyay@users.noreply.github.com>
2022-09-21 11:21:37 +0530

    Delete file3.js

--- file3.js
+++
@@ -1,7 +0,0 @@
-var firstName="suhani";  //camel case writing and
-//start with small letter
-console.log(firstName);
-firstName="shivani";
-console.log(firstName);
-var value1=2;
-console.log(value1**2);
